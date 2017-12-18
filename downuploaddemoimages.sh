344abc611a96e9f500788783b255952d840a9e8c
MUMOLisa <34642626+MUMOLisa@users.noreply.github.com>
2017-12-18 17:15:03 +0800

    Delete downuploaddemoimages.sh

--- downuploaddemoimages.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-K8S_HOME=/opt/kubernetes
-
-sh $K8S_HOME/scripts/downloadimages.sh -D $K8S_HOME/scripts -s demo -r cdfregistry.hpeswlab.net -u CoreTech -p 1Qaz2wsx && sh $K8S_HOME/scripts/uploadimages.sh || exit 1
